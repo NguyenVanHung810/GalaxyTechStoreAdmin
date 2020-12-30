f40d5694a6a38a90c45ee0a376c1aed1b121a3d9
NguyenDaiLam <lam.187pm13948@vanlanguni.vn>
2020-12-30 18:55:59 +0700

    Initial Commit

--- app/src/main/java/com/example/galaxytechstoreadmin/MainActivity.java
+++ app/src/main/java/com/example/galaxytechstoreadmin/MainActivity.java
@@ -7,8 +7,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
