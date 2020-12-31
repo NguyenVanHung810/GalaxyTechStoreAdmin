526212d5314ab4377379f8efb44519a7c7d621ca
nguyenduylan <lan.187pm06630@vanlanguni.vn>
2020-12-31 11:23:36 +0700

    Thao tác thực hiện load dữ liệu admin ra trang cập nhật thông tin người dùng. Đồng thời code cập nhật nếu chỉnh sửa một thông tin người dùng.

--- app/src/main/java/com/example/galaxytechstoreadmin/UpdateInfoFragment.java
+++ app/src/main/java/com/example/galaxytechstoreadmin/UpdateInfoFragment.java
@@ -1,64 +1,235 @@
 package com.example.galaxytechstoreadmin;
 
+import android.app.Dialog;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UpdateInfoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import es.dmoral.toasty.Toasty;
+
+
 public class UpdateInfoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String pattern = "[a-zA-Z0-9._-]+@[a-z]+.[a-z]+", user_name, user_email, user_phone;
+    private EditText email, name, password, phone;
+    private Button updateUserInfoBtn, doneBtn;
+    private Dialog loadingDialog, passwordDialog;
 
     public UpdateInfoFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UpdateInfoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UpdateInfoFragment newInstance(String param1, String param2) {
-        UpdateInfoFragment fragment = new UpdateInfoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_update_info, container, false);
+        name = view.findViewById(R.id.name_et);
+        email = view.findViewById(R.id.email_et);
+        phone = view.findViewById(R.id.sdt);
+        updateUserInfoBtn = view.findViewById(R.id.update_info_button);
+
+        //////////loading dialog
+        loadingDialog = new Dialog(getContext());
+        loadingDialog.setContentView(R.layout.loading_progress_dialog);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.slider_background));
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        //////////loading dialog
+
+        //////////password dialog
+        passwordDialog = new Dialog(getContext());
+        passwordDialog.setContentView(R.layout.password_confirmation_dialog);
+        passwordDialog.setCancelable(true);
+        passwordDialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.slider_background));
+        passwordDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        password = passwordDialog.findViewById(R.id.enter_password);
+        doneBtn = passwordDialog.findViewById(R.id.done_btn);
+        //////////password dialog
+
+        user_name = getArguments().getString("Name");
+        user_email = getArguments().getString("Email");
+        user_phone = getArguments().getString("Phone");
+
+        name.setText(user_name);
+        email.setText(user_email);
+        phone.setText(user_phone);
+
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+
+        updateUserInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkEmail();
+            }
+        });
+
+        return view;
+    }
+
+    private void checkInputs() {
+        if (!TextUtils.isEmpty(email.getText())) {
+            if (!TextUtils.isEmpty(name.getText())) {
+                updateUserInfoBtn.setEnabled(true);
+                updateUserInfoBtn.setTextColor(Color.rgb(255, 255, 255));
+            } else {
+                updateUserInfoBtn.setEnabled(false);
+                updateUserInfoBtn.setTextColor(Color.argb(50, 255, 255, 255));
+            }
+        } else {
+            updateUserInfoBtn.setEnabled(false);
+            updateUserInfoBtn.setTextColor(Color.argb(50, 255, 255, 255));
+        }
+    }
+
+    private void checkEmail() {
+        if (email.getText().toString().matches(pattern)) {
+            final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (email.getText().toString().toLowerCase().trim().equals(user_email.toLowerCase().trim())) {
+                loadingDialog.show();
+                Map<String, Object> userdata = new HashMap<>();
+                userdata.put("name", name.getText().toString());
+                userdata.put("email", email.getText().toString());
+                userdata.put("phonenumber", phone.getText().toString());
+                updateFields(user, userdata);
+            } else {  //update email
+                passwordDialog.show();
+                doneBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String userPass = password.getText().toString();
+                        passwordDialog.dismiss();
+                        loadingDialog.show();
+                        AuthCredential credential = EmailAuthProvider.getCredential(user_email, userPass);
+                        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    user.updateEmail(email.getText().toString().trim()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Map<String, Object> userdata = new HashMap<>();
+                                                userdata.put("name", name.getText().toString());
+                                                userdata.put("email", email.getText().toString());
+                                                userdata.put("phonenumber", phone.getText().toString());
+                                                updateFields(user, userdata);
+                                            } else {
+                                                loadingDialog.dismiss();
+                                                String error = task.getException().getMessage();
+                                                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                                } else {
+                                    loadingDialog.dismiss();
+                                    String error = task.getException().getMessage();
+                                    Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        } else {
+            email.setError("Invalid Email!");
+        }
+    }
+
+    private void updateFields(FirebaseUser user, final Map<String, Object> userdata){
+        FirebaseFirestore.getInstance().collection("ADMINS").document(user.getUid())
+                .update(userdata).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    if(userdata.size() >2){
+                        DBqueries.email=email.getText().toString().trim();
+                        DBqueries.fullname=name.getText().toString().trim();
+                    }else {
+                        DBqueries.fullname=name.getText().toString().trim();
+                        DBqueries.email=email.getText().toString();
+                    }
+                    getActivity().finish();
+                    Toasty.success(getContext(), "Đã cập nhật thành công !!!", Toast.LENGTH_SHORT, true).show();
+                }else {
+                    String error=task.getException().getMessage();
+                    Toasty.error(getContext(), error,Toasty.LENGTH_SHORT).show();
+                }
+                loadingDialog.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/galaxytechstoreadmin/UpdateUserInfoActivity.java
+++ app/src/main/java/com/example/galaxytechstoreadmin/UpdateUserInfoActivity.java
@@ -8,7 +8,10 @@ import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -24,5 +27,72 @@ public class UpdateUserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_user_info);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Thông tin tài khoản");
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        frameLayout = findViewById(R.id.frame_layout);
+
+        updateInfoFragment = new UpdateInfoFragment();
+        updatePasswordFragment = new UpdatePasswordFragment();
+
+        name = getIntent().getStringExtra("Name");
+        email = getIntent().getStringExtra("Email");
+        phone = getIntent().getStringExtra("Phone");
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    setFragment(updateInfoFragment, true);
+                }
+                if (tab.getPosition() == 1) {
+                    setFragment(updatePasswordFragment, false);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        tabLayout.getTabAt(0).select();
+        setFragment(updateInfoFragment, true);
+    }
+
+    private void setFragment(Fragment fragment, boolean setBundle) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+        if (setBundle) {
+            Bundle bundle = new Bundle();
+            bundle.putString("Name", name);
+            bundle.putString("Email", email);
+            bundle.putString("Phone", phone);
+            fragment.setArguments(bundle);
+        } else {
+            Bundle bundle = new Bundle();
+            bundle.putString("Email", email);
+            fragment.setArguments(bundle);
+        }
+        fragmentTransaction.replace(frameLayout.getId(), fragment);
+        fragmentTransaction.commit();
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
