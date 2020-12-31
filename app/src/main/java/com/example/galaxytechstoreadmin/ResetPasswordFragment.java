7a24552b5209ddea3addeed989f7b3029fab64d9
ngthienan <an.187it20649@vanlanguni.vn>
2020-12-31 10:19:10 +0700

    Thực hiện code chức năng reset password khi admin quên mật khẩu.

--- app/src/main/java/com/example/galaxytechstoreadmin/ResetPasswordFragment.java
+++ app/src/main/java/com/example/galaxytechstoreadmin/ResetPasswordFragment.java
@@ -2,63 +2,116 @@ package com.example.galaxytechstoreadmin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ResetPasswordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class ResetPasswordFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText email;
+    private Button send;
+    private ImageButton back;
+    private FirebaseAuth firebaseAuth;
 
     public ResetPasswordFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ResetPasswordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ResetPasswordFragment newInstance(String param1, String param2) {
-        ResetPasswordFragment fragment = new ResetPasswordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+        View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+        email = (EditText) view.findViewById(R.id.edtEmail_Registered);
+        send = (Button) view.findViewById(R.id.btnSend);
+        back = (ImageButton) view.findViewById(R.id.btnBack);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInput();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFragment(new SignInFragment());
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send.setEnabled(false);
+                firebaseAuth.sendPasswordResetEmail(email.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            toastInfo("Email sent successfully !!!");
+                        }
+                        else{
+                            toastInfo(task.getException().getMessage());
+                        }
+                        send.setEnabled(true);
+                    }
+                });
+            }
+        });
+    }
+
+    private void checkInput() {
+        if(TextUtils.isEmpty(email.getText().toString())){
+            send.setEnabled(false);
+        }
+        else {
+            send.setEnabled(true);
+        }
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(R.anim.slide_from_right, R.anim.slideout_from_right);
+        fragmentTransaction.replace(R.id.login_register_resetpassword_layout, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    private void toastInfo(String str){
+        Toast.makeText(getActivity(), str, Toast.LENGTH_LONG).show();
     }
 }
