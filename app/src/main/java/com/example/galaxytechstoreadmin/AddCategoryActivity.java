6b0245dcda0e5ec8809160ba35fdf7787705c64c
NguyenDaiLam <lam.187pm13948@vanlanguni.vn>
2021-01-02 08:48:37 +0700

    Thực hiện code chức năng thêm mới một danh mục vào danh sách.

--- app/src/main/java/com/example/galaxytechstoreadmin/AddCategoryActivity.java
+++ app/src/main/java/com/example/galaxytechstoreadmin/AddCategoryActivity.java
@@ -1,14 +1,262 @@
 package com.example.galaxytechstoreadmin;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.ImageViewTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import es.dmoral.toasty.Toasty;
 
 public class AddCategoryActivity extends AppCompatActivity {
+
+    private EditText name;
+    private Button save, cancel;
+    private ImageView image;
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private ImageButton delete_img, change_img;
+    private Uri uri;
+    private Boolean updatePhoto = false;
+    private Dialog loadingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Thêm mới danh mục");
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        loadingDialog = new Dialog(getApplicationContext());
+        loadingDialog.setContentView(R.layout.loading_progress_dialog);
+        loadingDialog.setCancelable(false);
+        loadingDialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.slider_background));
+        loadingDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        name = (EditText) findViewById(R.id.add_cate_name);
+        image = (ImageView) findViewById(R.id.add_cate_image);
+        delete_img = (ImageButton) findViewById(R.id.delete_img_btn);
+        change_img = (ImageButton) findViewById(R.id.change_img_btn);
+
+        save = (Button) findViewById(R.id.add_btn);
+        cancel = (Button) findViewById(R.id.cancel_btn);
+
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        delete_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uri = null;
+                updatePhoto = true;
+                Glide.with(getApplicationContext()).load(R.drawable.no_img).into(image);
+            }
+        });
+
+        change_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                        galleryIntent.setType("image/*");
+                        startActivityForResult(galleryIntent, 1);
+                    } else {
+                        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 2);
+                    }
+                } else {
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                    galleryIntent.setType("image/*");
+                    startActivityForResult(galleryIntent, 1);
+                }
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Long gt = DBqueries.cate_index + (long) 1;
+                updatePic(gt);
+            }
+        });
+    }
+
+    private void checkInputs() {
+        if (!TextUtils.isEmpty(name.getText())) {
+            if (!TextUtils.isEmpty(name.getText())) {
+                save.setEnabled(true);
+                save.setTextColor(Color.rgb(255, 255, 255));
+            } else {
+                save.setEnabled(false);
+                save.setTextColor(Color.argb(50, 255, 255, 255));
+            }
+        } else {
+            save.setEnabled(false);
+            save.setTextColor(Color.argb(50, 255, 255, 255));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                if (data != null) {
+                    uri = data.getData();
+                    updatePhoto = true;
+                    Glide.with(getApplicationContext()).load(uri).into(image);
+                } else {
+                    Toasty.error(getApplicationContext(), "Image not found!", Toast.LENGTH_SHORT, true).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 2) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, 1);
+            } else {
+                Toasty.error(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT, true).show();
+            }
+        }
+    }
+
+    private void updatePic(Long gt) {
+        final StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("category_image/" + "cate_image_"+String.valueOf(gt) + ".jpg");
+        Glide.with(getApplicationContext()).asBitmap().load(uri).centerCrop().into(new ImageViewTarget<Bitmap>(image) {
+            @Override
+            public void onResourceReady(@NonNull Bitmap resource, @Nullable Transition<? super Bitmap> transition) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                resource.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] data = baos.toByteArray();
+                UploadTask uploadTask = storageReference.putBytes(data);
+                uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Uri> task) {
+                                    if (task.isSuccessful()) {
+                                        uri = task.getResult();
+                                        Map<String, Object> product = new HashMap<>();
+                                        product.put("index", DBqueries.cate_index + 1);
+                                        product.put("icon", uri.toString());
+                                        product.put("categoryName", name.getText().toString());
+                                        addFields(product);
+                                    } else {
+                                        loadingDialog.dismiss();
+                                        String error = task.getException().getMessage();
+                                        Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        } else {
+                            loadingDialog.dismiss();
+                            String error = task.getException().getMessage();
+                            Toast.makeText(getApplicationContext(), error, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                return;
+            }
+            @Override
+            protected void setResource(@Nullable Bitmap resource) {
+                image.setImageResource(R.drawable.no_img);
+            }
+        });
+    }
+
+    private void addFields(final Map<String, Object> categoryData){
+        db.collection("CATEGORIES").add(categoryData)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful()) {
+                            DBqueries.categoryModelList.clear();
+                            CategoryFragment.categoryAdapter.notifyDataSetChanged();
+                            Toasty.success(getApplicationContext(), "Thêm mới một danh mục thành công !", Toast.LENGTH_SHORT, true).show();
+                            finish();
+                        } else {
+                            Toasty.error(getApplicationContext(), "Thêm danh mục thất bại", Toast.LENGTH_SHORT, true).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+        super.onBackPressed();
     }
 }
